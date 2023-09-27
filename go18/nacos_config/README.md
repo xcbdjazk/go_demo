a914ff4484ddd00b36d5c02db971db87b6ba7619
maocat <zk246422@163.com>
2023-09-27 16:57:37 +0800

    add: nacos_config

---
+++ go18/nacos_config/README.md
@@ -0,0 +1,8 @@
+
+
+
+#### docker nacos
+```shell
+
+docker run --name nacos -e MODE=standalone -p 8848:8848 -p 9848:9848 -d nacos/nacos-server
+```
--- go18/nacos_config/demo1/main.go
+++ go18/nacos_config/demo1/main.go
@@ -1,1 +1,78 @@
-package demo1
+package main
+
+import (
+	"fmt"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+)
+
+// NACOS 2.2.2
+func main() {
+
+	//create clientConfig
+	clientConfig := constant.ClientConfig{
+		NamespaceId:         "", //we can create multiple clients with different namespaceId to support multiple namespace.When namespace is public, fill in the blank string here.
+		TimeoutMs:           500,
+		NotLoadCacheAtStart: true,
+		LogDir:              ".",
+		CacheDir:            ".",
+		LogLevel:            "debug",
+		Username:            "nacos",
+		Password:            "123456",
+	}
+
+	// At least one ServerConfig
+	serverConfigs := []constant.ServerConfig{
+		{
+			IpAddr:      "172.25.89.138",
+			ContextPath: "/nacos",
+			Port:        8847,
+			Scheme:      "http",
+		},
+		//{
+		//	IpAddr:      "console2.nacos.io",
+		//	ContextPath: "/nacos",
+		//	Port:        80,
+		//	Scheme:      "http",
+		//},
+	}
+
+	configClient, err := clients.NewConfigClient(
+		vo.NacosClientParam{
+			ClientConfig:  &clientConfig,
+			ServerConfigs: serverConfigs,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	ok, err := configClient.PublishConfig(vo.ConfigParam{
+		DataId:  "config_pre1.yaml",
+		Group:   "DEFAULT_GROUP",
+		Content: `ddd:1234`,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(ok)
+	data, err := configClient.GetConfig(vo.ConfigParam{
+		DataId: "config_pre1.yaml",
+		Group:  "DEFAULT_GROUP",
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(data)
+	err = configClient.ListenConfig(vo.ConfigParam{
+		DataId: "config_pre1.yaml",
+		Group:  "DEFAULT_GROUP",
+		OnChange: func(namespace, group, dataId, data string) {
+			fmt.Println("group:" + group + ", dataId:" + dataId + ", data:" + data)
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	select {}
+}
